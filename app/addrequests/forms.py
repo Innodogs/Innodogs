0663e8b1cb1a1e258c64f62bffb5892faee4e183
Mikhail Putilov <post.snowy@gmail.com>
2016-10-30 00:51:48 +0300

    Add render_field helper. Add validation handling.

--- app/addrequests/forms.py
+++ app/addrequests/forms.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from wtforms import TextAreaField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 
 class AddRequestForm(FlaskForm):
     description = TextAreaField('Description', validators=[
-        DataRequired()
+        DataRequired(),
+        Length(min=4)
     ])
     pictures = FileField('Pictures', validators=[
         FileAllowed(['jpg', 'png'], 'Images only!')
--- app/addrequests/routes.py
+++ app/addrequests/routes.py
@@ -1,28 +1,36 @@
+import os
+
 from flask import flash, redirect, url_for
-from flask import render_template
+from flask import render_template, current_app
+from werkzeug.utils import secure_filename
 
 from app.addrequests import add_requests
 from app.addrequests.forms import AddRequestForm
 from app.addrequests.repository import AddRequestsRepository
-from werkzeug.utils import secure_filename
-
-UPLOADS_FOLDER = '../uploads/'
 
 __author__ = 'Xomak'
 
 
-@add_requests.route('/', methods=['GET', 'POST'])
+@add_requests.route('/', methods=['GET'])
 def requests_list():
     r = AddRequestsRepository.get_all_add_requests()
     return render_template('addrequests/list.html', add_requests=r)
 
 
-@add_requests.route('/add-request-form/', methods=('GET', 'POST'))
-def add_request_form():
+@add_requests.route('/', methods=['POST'])
+def submit_add_request_form():
     form = AddRequestForm()
     if form.validate_on_submit():
         filename = secure_filename(form.pictures.data.filename)
-        form.pictures.data.save(UPLOADS_FOLDER + filename)
+        if form.pictures.data.filename != '':
+            form.pictures.data.save(
+                os.path.join(current_app.config['UPLOAD_FOLDER'], filename))  # current_app -> probably fine
         flash('Thank you for your submission', 'info')
         return redirect(url_for('main.index'))
+    return render_template('addrequests/add-request-form.html', form=form)
+
+
+@add_requests.route('/add-form', methods=['GET'])
+def add_request_form():
+    form = AddRequestForm()
     return render_template('addrequests/add-request-form.html', form=form)
