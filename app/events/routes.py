023ec8234b563177f8f79ad097ca3215137a176e
Mikhail Putilov <post.snowy@gmail.com>
2016-11-10 14:41:23 +0300

    Add "crud" operations for volunteer only #2

--- app/events/routes.py
+++ app/events/routes.py
@@ -1,9 +1,13 @@
 from flask import request
+from flask_login import login_required
 
+from app.users.utils import requires_roles
 from . import events
 
 
 @events.route('/edit/<int:event_id>', methods=['GET', 'POST'])
+@login_required
+@requires_roles('volunteer')
 def edit_event(event_id: int):
     if request.method == 'GET':
         return "edit form" + str(event_id)
@@ -12,6 +16,8 @@ def edit_event(event_id: int):
 
 
 @events.route('/', methods=['GET', 'POST'])
+@login_required
+@requires_roles('volunteer')
 def new_event():
     if request.method == 'GET':
         return "new form"
@@ -21,6 +27,8 @@ def new_event():
 
 # todo: исправить потом methods, после подключения javascript который будет пулять правильные методы для реквестов
 @events.route('/delete/<int:event_id>', methods=['GET', 'POST'])
+@login_required
+@requires_roles('volunteer')
 def delete_event(event_id: int):
     if request.method == 'GET':
         return "delete form"
