8bffe742d769b7e92e4b61a867f2f5f0d9a6c875
Stas Mikhel <me@mail.ru>
2016-11-14 21:49:40 +0300

    Add form for new dog without request

--- app/dogs/routes.py
+++ app/dogs/routes.py
@@ -1,8 +1,14 @@
+from datetime import datetime
+
 from flask import abort
-from flask import render_template
+from flask import render_template, url_for, redirect
 
 from . import dogs
 from .repository import DogsRepository
+from .models import Dog
+from app.addrequests.forms import ApproveRequestForm
+from app.addrequests.utils import convert_locations_to_select_choices
+from app.locations.repository import LocationsRepository
 
 __author__ = 'Xomak'
 
@@ -19,3 +25,20 @@ def page_about_dog(dog_id):
     if dog_data is None:
         abort(404)
     return render_template('dogs/page.html', dog=dog_data)
+
+@dogs.route('/add', methods=['GET','POST'])
+def add_dog_without_request():
+    add_dog_form = ApproveRequestForm()
+    locations = LocationsRepository.get_all_locations()
+    add_dog_form.location.choices = convert_locations_to_select_choices(locations)
+    if add_dog_form.validate_on_submit():
+        dog = Dog()
+        dog.name = add_dog_form.name.data
+        dog.sex = add_dog_form.sex.data
+        dog.description = add_dog_form.description.data
+        dog.is_hidden = add_dog_form.is_hidden.data
+        dog.is_adopted = add_dog_form.is_adopted.data
+        DogsRepository.new_dog(dog)
+        return redirect(url_for('.dogs_list'))
+    return render_template('dogs/add-new.html', date=datetime.now(), add_dog_form=add_dog_form)
+
