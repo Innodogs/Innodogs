35dba7e1e8b46509a399861e9df291930dbbc0a3
Mikhail Putilov <post.snowy@gmail.com>
2016-11-17 17:16:06 +0300

    Fix config with upload folder

--- config/development.py
+++ config/development.py
@@ -6,3 +6,4 @@ DEBUG=True
 SQLALCHEMY_DATABASE_URI=''
 SQLALCHEMY_TRACK_MODIFICATIONS=False # This feature is not needed in our app, but default value raises warnings
 DOGS_PER_PAGE=10
+UPLOAD_FOLDER='dogs'
