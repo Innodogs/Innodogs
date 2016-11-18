3d9ad9f6485401ab03225a6b944130e347a7736e
Nikita Zhuchkov <n.zhuchkov@innopolis.ru>
2016-11-18 12:41:30 +0300

    Fixed index redirect

--- app/main/routes.py
+++ app/main/routes.py
@@ -10,8 +10,4 @@ __author__ = 'Xomak'
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    all_dogs = DogsRepository.get_all_dogs()
     return redirect("/dogs", code=302)
-    #return render_template('dogs/list.html', dogs=all_dogs, authorization_url=google_login.authorization_url())
-    #return render_template('base.html', authorization_url=google_login.authorization_url())
-
