a034daac86cdf41323197d87d78715d8a53246ed
Xomak <mail@xomak.net>
2016-11-20 12:20:04 +0300

    Added sublocations cycle detecting methods

--- app/locations/repository.py
+++ app/locations/repository.py
@@ -18,3 +18,34 @@ class LocationsRepository:
                             table=LocationMapping.description))
         result = db.session.query(Location).from_statement(stmt).all()
         return result
+
+    @classmethod
+    def is_cyclic_dependency(cls, location_id: int, parent_id: int) -> bool:
+        """
+        Checks, whether ou will be cyclic dependency, if location with location id is have
+        given parent_id.
+        :param location_id: Location id of checked node
+        :param parent_id: Potential parent id for thid node
+        :return: True, if cyclic dependency will take place, False - otherwise
+        """
+        return cls.is_sublocation(parent_id, location_id)
+
+    @classmethod
+    def is_sublocation(cls, checked_id: int, location_id: int) -> bool:
+        """
+        Checks, whether location with given checked_id is sublocation
+        location with given location_id
+        :param checked_id:
+        :param location_id:
+        :return: True, if location with given checked_id is a sublocation of
+        location with given location_id
+        """
+        stmt = text("WITH RECURSIVE r AS "
+                    "(SELECT id, parent_id FROM {locations_table} AS locations WHERE parent_id = :location_id "
+                    "UNION "
+                    "SELECT locations.id, locations.parent_id FROM {locations_table} AS locations "
+                    "JOIN r ON locations.parent_id = r.id) "
+                    "SELECT count(*) FROM r WHERE id = :checked_id"
+                    .format(locations_table=LocationMapping.description))
+        result = db.engine.execute(stmt.params(location_id=location_id, checked_id=checked_id))
+        return True if next(iter(result))[0] != 0 else False
